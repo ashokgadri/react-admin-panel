557240bed89bf737cc424c587359252a68115561
Ashok Gadri <ashok.gadri@gmail.com>
2021-03-14 13:55:07 +0530

    Added the icon on login

--- src/layouts/Auth/components/TopBar/TopBar.js
+++ src/layouts/Auth/components/TopBar/TopBar.js
@@ -1,12 +1,22 @@
-import { AppBar, Toolbar } from '@material-ui/core'
-import React from 'react'
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import React from "react";
+import DashboardOutlinedIcon from '@material-ui/icons/DashboardOutlined';
 
 function TopBar() {
-    return (
-        <AppBar color="primary">
-            <Toolbar></Toolbar>
-        </AppBar>
-    )
+  return (
+    <AppBar color="primary">
+      <Toolbar>
+        <DashboardOutlinedIcon />
+        <Typography
+          variant="h5"
+          color="inherit"
+          style={{ paddingLeft: "10px" }}
+        >
+          React Admin Panel
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default TopBar;
